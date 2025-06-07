1bc2af1d2ba91843b80a08a00e878825349bc448
fatemeh <fatemeh@fatemehs-MacBook-Air.local>
2025-06-07 23:25:05 +0330

    PlantsVsZombies

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(loadFXML("Welcome"), 900, 700);
+        stage.setScene(scene);
+        stage.setTitle("Welcome");
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+
+    public static Scene getScene() {
+        return scene;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
